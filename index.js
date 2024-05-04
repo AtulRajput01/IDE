04e6d1eab3ee3b614f1e9a915c024b974a240ad9
AtulRajput01 <atulrajput.work@gmail.com>
2024-05-04 23:49:47 +0530

    use socket

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
-
-const port = 3000
+const http = require('http')
+const { Server: SocketServer } = require('socket.io')
 
 const app = express();
+const Server = http.createServer(app);
+const io = new SocketServer({
+    cors: '*'
+})
+
+io.attach(server);
+
+io.on('connection', (socket) => {
+    console.log('socket connected', socket.io)
+})
 
 app.get('/', function(req, res){
     res.send("hi this is my first web")
 })
 
-app.listen(port)
+server.listen(9000, () => console.log('docker server is running on port 9000'))
